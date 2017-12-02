b3250135813fe2aca7d8c066bd38464a78a26e70
Dexter <pogeraf2001@gmail.com>
2017-12-02 08:40:43 +0200

    Frontend

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,144 @@
-<h1 align="center">Homepage</h1>
+<h1 align="center">New uploaded images</h1>
+
+<div class="new-uploaded">
+
+    <div class="grid cs-style-1">
+        <li>
+            <figure>
+                <img src="http://bipbap.ru/wp-content/uploads/2017/04/1160334673_2.jpg" alt="img01">
+                <figcaption>
+                    <h3>Camera</h3>
+                    <span>Jacob Cummings</span>
+                    <a href="">Take a look</a>
+                </figcaption>
+            </figure>
+        </li>
+    </div>
+    <div class="grid cs-style-1">
+        <li>
+            <figure>
+                <img src="http://bipbap.ru/wp-content/uploads/2017/04/1160334673_2.jpg" alt="img01">
+                <figcaption>
+                    <h3>Camera</h3>
+                    <span>Jacob Cummings</span>
+                    <a href="">Take a look</a>
+                </figcaption>
+            </figure>
+        </li>
+    </div>
+    <div class="grid cs-style-1">
+        <li>
+            <figure>
+                <img src="http://bipbap.ru/wp-content/uploads/2017/04/1160334673_2.jpg" alt="img01">
+                <figcaption>
+                    <h3>Camera</h3>
+                    <span>Jacob Cummings</span>
+                    <a href="">Take a look</a>
+                </figcaption>
+            </figure>
+        </li>
+
+    </div>
+    <div class="grid cs-style-1">
+        <li>
+            <figure>
+                <img src="http://bipbap.ru/wp-content/uploads/2017/04/1160334673_2.jpg" alt="img01">
+                <figcaption>
+                    <h3>Camera</h3>
+                    <span>Jacob Cummings</span>
+                    <a href="">Take a look</a>
+                </figcaption>
+            </figure>
+        </li>
+
+    </div>
+    <div class="grid cs-style-1">
+        <li>
+            <figure>
+                <img src="http://bipbap.ru/wp-content/uploads/2017/04/1160334673_2.jpg" alt="img01">
+                <figcaption>
+                    <h3>Camera</h3>
+                    <span>Jacob Cummings</span>
+                    <a href="">Take a look</a>
+                </figcaption>
+            </figure>
+        </li>
+
+    </div>
+    <div class="grid cs-style-1">
+        <li>
+            <figure>
+                <img src="http://bipbap.ru/wp-content/uploads/2017/04/1160334673_2.jpg" alt="img01">
+                <figcaption>
+                    <h3>Camera</h3>
+                    <span>Jacob Cummings</span>
+                    <a href="">Take a look</a>
+                </figcaption>
+            </figure>
+        </li>
+
+    </div>
+    <div class="grid cs-style-1">
+        <li>
+            <figure>
+                <img src="http://bipbap.ru/wp-content/uploads/2017/04/1160334673_2.jpg" alt="img01">
+                <figcaption>
+                    <h3>Camera</h3>
+                    <span>Jacob Cummings</span>
+                    <a href="">Take a look</a>
+                </figcaption>
+            </figure>
+        </li>
+
+    </div>
+    <div class="grid cs-style-1">
+        <li>
+            <figure>
+                <img src="http://bipbap.ru/wp-content/uploads/2017/04/1160334673_2.jpg" alt="img01">
+                <figcaption>
+                    <h3>Camera</h3>
+                    <span>Jacob Cummings</span>
+                    <a href="">Take a look</a>
+                </figcaption>
+            </figure>
+        </li>
+    </div>
+</div>
+
+<div class="all-uploaded">
+    <h1 align="center">All uploaded images</h1>
+    <img src="http://bipbap.ru/wp-content/uploads/2017/04/1160334673_2.jpg" class="uploaded-images">
+
+
+    <img src="http://cdn.fishki.net/upload/post/2016/12/31/2180056/tn/8-landscapes.png" class="uploaded-images">
+    <img src="http://mirpozitiva.ru/uploads/posts/2016-08/1472042978_32.jpg" class="uploaded-images">
+    <img src="http://www.gogetnews.info/uploads/posts/2016-06/1466321218_krasivaya-zmeya.jpg" class="uploaded-images">
+    <img src="http://www.porjati.ru/uploads/posts/2012-12/thumbs/1355131464_awesome_photos_14.jpg" class="uploaded-images">
+    <img src="http://bipbap.ru/wp-content/uploads/2017/10/0_8eb56_842bba74_XL.jpg" class="uploaded-images">
+    <img src="http://www.origins.org.ua/pictures/photo_fish_2.jpg" class="uploaded-images">
+    <img src="http://napirse.ru/pic/6701269011070149.jpg" class="uploaded-images">
+    <img src="http://bipbap.ru/wp-content/uploads/2017/04/1160334673_2.jpg" class="uploaded-images">
+    <img src="http://cdn.fishki.net/upload/post/2016/12/31/2180056/tn/8-landscapes.png" class="uploaded-images">
+    <img src="http://mirpozitiva.ru/uploads/posts/2016-08/1472042978_32.jpg" class="uploaded-images">
+    <img src="http://www.gogetnews.info/uploads/posts/2016-06/1466321218_krasivaya-zmeya.jpg" class="uploaded-images">
+    <img src="http://www.porjati.ru/uploads/posts/2012-12/thumbs/1355131464_awesome_photos_14.jpg" class="uploaded-images">
+    <img src="http://bipbap.ru/wp-content/uploads/2017/10/0_8eb56_842bba74_XL.jpg" class="uploaded-images">
+    <img src="http://www.origins.org.ua/pictures/photo_fish_2.jpg" class="uploaded-images">
+    <img src="http://napirse.ru/pic/6701269011070149.jpg" class="uploaded-images">
+    <img src="http://bipbap.ru/wp-content/uploads/2017/04/1160334673_2.jpg" class="uploaded-images">
+    <img src="http://cdn.fishki.net/upload/post/2016/12/31/2180056/tn/8-landscapes.png" class="uploaded-images">
+    <img src="http://mirpozitiva.ru/uploads/posts/2016-08/1472042978_32.jpg" class="uploaded-images">
+    <img src="http://www.gogetnews.info/uploads/posts/2016-06/1466321218_krasivaya-zmeya.jpg" class="uploaded-images">
+    <img src="http://www.porjati.ru/uploads/posts/2012-12/thumbs/1355131464_awesome_photos_14.jpg" class="uploaded-images">
+    <img src="http://bipbap.ru/wp-content/uploads/2017/10/0_8eb56_842bba74_XL.jpg" class="uploaded-images">
+    <img src="http://www.origins.org.ua/pictures/photo_fish_2.jpg" class="uploaded-images">
+    <img src="http://napirse.ru/pic/6701269011070149.jpg" class="uploaded-images">
+    <img src="http://bipbap.ru/wp-content/uploads/2017/04/1160334673_2.jpg" class="uploaded-images">
+    <img src="http://cdn.fishki.net/upload/post/2016/12/31/2180056/tn/8-landscapes.png" class="uploaded-images">
+    <img src="http://mirpozitiva.ru/uploads/posts/2016-08/1472042978_32.jpg" class="uploaded-images">
+    <img src="http://www.gogetnews.info/uploads/posts/2016-06/1466321218_krasivaya-zmeya.jpg" class="uploaded-images">
+    <img src="http://www.porjati.ru/uploads/posts/2012-12/thumbs/1355131464_awesome_photos_14.jpg" class="uploaded-images">
+    <img src="http://bipbap.ru/wp-content/uploads/2017/10/0_8eb56_842bba74_XL.jpg" class="uploaded-images">
+    <img src="http://www.origins.org.ua/pictures/photo_fish_2.jpg" class="uploaded-images">
+    <img src="http://napirse.ru/pic/6701269011070149.jpg" class="uploaded-images">
+</div>
